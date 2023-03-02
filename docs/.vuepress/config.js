ec50d94f76995fd8b6dbde31af9aa54a2eba0b2a
dhj <17613071153@163.com>
2023-03-02 20:13:38 +0800

    feat:左侧导航完善

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,7 +16,7 @@ module.exports = {
       { text: "首页", link: "/" },
       { text: "碎碎念", link: "/docs/life/" },
       { text: "JS基础", link: "/docs/JS/" },
-      { text: "前端进阶", link: "/docs/jingjie/" },
+      { text: "前端进阶", link: "/docs/jinjie/" },
       // { text: "LeeCode", link: "/leeCode/" },
       // { text: "专栏", link: "/column/" },
       // { text: "时光轴", link: "/docs/timeaxle/" },
@@ -38,41 +38,39 @@ module.exports = {
           ]
         }
       ],
-
-      '/JS/': [
-        {
-          title:'扎实的JS基础是每个前端吃饭的东西',
-          collapsable: false,
-          children:[
-            {
-              title:'3-02',
-              path:'/docs/life/3-02'
-            },
-            {
-              title:'3-03',
-              path:'/docs/life/3-03'
-            },
-          ]
-        }
-      ],
-
-      // fallback
-      '/jinjie/': [
+      '/docs/jinjie/': [
         {
           title:'前端进阶知识',
           collapsable: false,
           children:[
             {
-              title:'3-02',
-              path:'/docs/life/3-02'
+              title:'PMP',
+              path:'/docs/jinjie/PMP'
+            },
+            // {
+            //   title:'3-03',
+            //   path:'/docs/jinjie/3-03'
+            // },
+          ]
+        }
+      ],
+      '/docs/JS/': [
+        {
+          title:'扎实的JS基础是每个前端吃饭的东西',
+          collapsable: false,
+          children:[
+            {
+              title:'数据类型',
+              path:'/docs/JS/数据类型'
             },
             {
-              title:'3-03',
-              path:'/docs/life/3-03'
+              title:'ES6',
+              path:'/docs/JS/ES6'
             },
           ]
         }
-      ]
+      ],
+      
     }
   },
 };
